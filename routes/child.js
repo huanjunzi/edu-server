8dd38b56ce23edca779802cbb88f7db82897cc7e
huanjunzi <hjz@epeijing.cn>
2018-11-22 18:26:07 +0800

    Fixed

--- routes/child.js
+++ routes/child.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 const mysql = require('../lib/mysql.js')
 let _ = require('underscore')
-router.prefix('/api/member')
+router.prefix('/api/child')
 
 router.get('/findChild', async (ctx,next) => {
   let r = await mysql.findChild(ctx.request.query)
@@ -16,35 +16,35 @@ router.get('/findChild', async (ctx,next) => {
     message: "failed"
   }
 })
-// // 编辑或是创建客户
-// router.post('/editMember', async (ctx, next) => {
-//   const r = await mysql.editMember(ctx.request.query)
-//   if(r.length > 0){
-//     ctx.body = {
-//       message: "success",
-//       rows: r
-//     }
-//     return
-//   }
-//   ctx.body = {
-//     message: "failed"
-//   }
-// })
+// 编辑或是创建客户
+router.post('/editChild', async (ctx, next) => {
+  const r = await mysql.editChild(ctx.request.query)
+  if(r.length > 0){
+    ctx.body = {
+      message: "success",
+      rows: r
+    }
+    return
+  }
+  ctx.body = {
+    message: "failed"
+  }
+})
 
-// // 删除客户
-// router.post('/deleteMember', async (ctx, next) => {
-//   const r = await mysql.deleteMember(ctx.request.query)
-//   if(r.length > 0){
-//     ctx.body = {
-//       message: "success",
-//       rows: r
-//     }
-//     return
-//   }
-//   ctx.body = {
-//     message: "failed"
-//   }
-// })
+// 删除儿童
+router.post('/deleteChild', async (ctx, next) => {
+  const r = await mysql.deleteChild(ctx.request.query)
+  if(r.length > 0){
+    ctx.body = {
+      message: "success",
+      rows: r
+    }
+    return
+  }
+  ctx.body = {
+    message: "failed"
+  }
+})
 
 // 下载excel表格
 router.get('/downloadChildExcel', async (ctx, next) => {
